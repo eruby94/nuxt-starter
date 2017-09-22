74f337dd1a1a365e4c23207ddbd89fb923540042
Evan Ruby <evanruby94@gmail.com>
2017-09-22 13:42:57 -0400

    Replace pre tag with li

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,11 @@
 <template>
   <div>
     <h3>Cars</h3>
-    <!-- <ul>
-      <li v-for="car in allCars">
-        <nuxt-link :to="`car/${car.id}`">
-          {{ car.year }} {{ car.make }} {{ car.model }}
-        </nuxt-link>
+    <ul>
+      <li v-for="item in NeighborhoodMapList.mapListItems">
+        {{item}}
       </li>
-    </ul> -->
-    <pre>
-      {{NeighborhoodMapList}}
-    </pre>
+    </ul>
   </div>
 </template>
 
